7ec4c5a256273ce588b74adcf15a3eb9abb9a7aa
Arkadiy Illarionov <qarkai@gmail.com>
2023-07-30 18:58:49 +0300

    Replace MD5Context with GChecksum
    - Replace md5_get_digest with md5_get_string
    - Add md5_get_string_from_file
    - Add MD5_SIZE constant

--- src/ui_fileops.c
+++ src/ui_fileops.c
@@ -907,11 +907,10 @@ gboolean md5_get_digest_from_file_utf8(const gchar *path, guchar digest[16])
 
 gchar *md5_text_from_file_utf8(const gchar *path, const gchar *error_text)
 {
-    guchar digest[16];
-
-    if (!md5_get_digest_from_file_utf8(path, digest)) return g_strdup(error_text);
-
-    return md5_digest_to_text(digest);
+    gchar *pathl = path_from_utf8(path);
+    gchar *md5_text = md5_get_string_from_file(pathl);
+    g_free(pathl);
+    return md5_text ? md5_text : g_strdup(error_text);
 }
 
 
